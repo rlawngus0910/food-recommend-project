3e0f5291d4174303577ce09b84ebedad8098c5ce
Juhyun Kim <rlawngus0910@naver.com>
2021-05-10 02:14:06 +0900

    django 프로젝트 생성

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import my_settings
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ciys6uhaphrr-)hj_*v4-wsp--9-gui-a7(7a-zyl6m9h00gi7'
+SECRET_KEY = my_settings.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
