3fe35d8e7f4aa7904f8854f491cac61ace4ee921
Juhyun Kim <rlawngus0910@naver.com>
2021-05-18 22:43:14 +0900

    DB에 있는 전체 음식점 데이터 출력 (#2)

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.db import connection
 
-# Create your views here.
+def RestaurantListView(request):
+    try:
+        cursor = connection.cursor()
+
+        SQL = "SELECT id, name FROM restaurant"
+        result = cursor.execute(SQL)
+        datas = cursor.fetchall()
+
+        connection.commit()
+        connection.close()
+
+        restaurants = []
+
+        for data in datas:
+            row = {'id' : data[0],
+                   'name' : data[1]}
+
+            restaurants.append(row)
+
+    except:
+        connection.rollback()
+
+    return render(request, 'restaurants/restaurants_list.html', {'restaurants' : restaurants})
