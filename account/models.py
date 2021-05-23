240e1c6bf8cbbc7cfaef0bf8f32a3d7de2a623bd
Juhyun Kim <rlawngus0910@naver.com>
2021-05-23 22:49:25 +0900

    account 앱 생성 (로그인 기능), 회원 model 생성

---
+++ account/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Account(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    password = models.CharField(max_length=100)
+    birth = models.DateTimeField()
+    gender = models.CharField(max_length=10)g
+
+    class Meta:
+        db_table = "accounts"
