08ef86407b5beda634c640f5962b8bc94caea184
Juhyun Kim <rlawngus0910@naver.com>
2021-05-24 23:41:48 +0900

    회원가입 기능 70% 구현

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from .models import Account
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'account/register.html')
+
+    elif request.method == "POST":
+        id = request.POST.get('id', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re_password', None)
+        res_data = {}
+        if not (id and password and re_password):
+            res_data['error'] = "모든 값을 입력해야 합니다."
+        elif password != re_password:
+            res_data['error'] = "비밀번호가 다릅니다."
+        else:
+            user = Account(id=id, password=make_password(password), birth="2019-01-01", gender="남자")
+            user.save()
+        return render(request, 'account/register.html', res_data)
