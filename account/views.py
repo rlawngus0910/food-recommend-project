bb8999c334b053c1207920e0bb6814fd5a6b68c3
Juhyun Kim <rlawngus0910@naver.com>
2021-06-01 22:06:07 +0900

    회원가입 템플릿과 회원 DB 연동

--- account/views.py
+++ account/views.py
@@ -1,26 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
-from .models import Account
-
+from .models import Accounts
+from django.db import connection
 
 # Create your views here.
 
 def register(request):
     if request.method == "GET":
-        return render(request, 'account/register.html')
+        return render(request, 'account/register3.html')
 
     elif request.method == "POST":
-        id = request.POST.get('id', None)
+        id = request.POST.get('id')
         password = request.POST.get('password', None)
         re_password = request.POST.get('re_password', None)
+        email = request.POST.get('e-mail', None)
+        nickname = request.POST.get('nickname', None)
+        address = request.POST.get('address', None)
+        birth = request.POST.get('birth', None)
+        gender = request.POST.get('gender', None)
+
         res_data = {}
-        if not (id and password and re_password):
-            res_data['error'] = "모든 값을 입력해야 합니다."
-        elif password != re_password:
-            res_data['error'] = "비밀번호가 다릅니다."
+
+        if password != re_password:
+            return render(request, 'account/register3.html', {"message" : "입력한 비밀번호가 다릅니다."})
+
         else:
-            res_data['success'] = "회원가입이 완료되었습니다."
-            user = Account(id=id, password=make_password(password), birth="2019-01-01", gender="남자")
-            user.save()
-        return render(request, 'account/register.html', res_data)
+
+            try:
+                cursor = connection.cursor()
+                password = make_password(password)
+                SQL = "INSERT INTO accounts (id,password,email,nickname,address,birth,gender) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(SQL, [id, password, email, nickname, address, birth, gender])
+
+                connection.commit()
+                connection.close()
+                return render(request, '../../mainpage/templates/mainpage/index.html', {"message" : "회원가입이 완료되었습니다."})
+
+            except:
+                connection.rollback()
+                return render(request, 'account/register3.html', {"message" : "DB 오류 발생"})
+
+
